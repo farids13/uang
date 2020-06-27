28f916a75824cb5cce119541c7c25d8be430823f
farids13 <faridsatria24@gmail.com>
2020-06-27 17:19:10 +0700

    Setting baru dari hp

--- ltc/bot.sh
+++ ltc/bot.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 bot='
 +6287844218361	
-+6287762901160	
-+6285335573673	
-+6281908230617	
-+62881026945202	
-+6285731019006	
-+6287701359057	
-+6281229084819	
-+6282333303498
-+6285812624644	
-+6287740344734
-+6282331330243	
-+6285755291619	
-+6287754762176	
-	'
-#arik 1 bee
-#bapak	bee
-#bapak2	bee
-#bapak3 bee
-#belbol1 bee
-#erwin
-#ihang	bee
-#johan
-#maylinda   
-#meeh
-#nadia      
-#muhammad
++6287762901160	       
++6285335573673	       
++6281908230617	       
++62881026945202	      
++6285731019006	       
++6287701359057	       
++6281229084819	       
++6282333303498	       
++6285812624644	       
++6287740344734	       
++6282331330243	       
++6281939571816         	
++6285755291619	       
++6287754762176'         	
+                       
+                       
+#arik 1 bee            
+#bapak	bee            
+#bapak2	bee           
+#bapak3 bee            
+#belbol1 bee           
+#erwin   
+#ihang	bee            
+#johan                 
+#maylinda   bee
+#meeh                  
+#nadia bee       
+#muhammad bee
+#om nafi               
 #rio	bee
-#wahed1 bee
-
+#wahed1		bee
 
 for ulang in $bot ; do
 	echo "$ulang"
