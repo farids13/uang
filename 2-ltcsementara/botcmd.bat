2857535f832bf362a8313bad2c0a0b9e28b0128c
farids13 <faridsatria24@gmail.com>
2020-07-03 07:31:41 +0700

    nomor anyar

--- 2-ltcsementara/botcmd.bat
+++ 2-ltcsementara/botcmd.bat
@@ -1,7 +1,12 @@
-set hitung= +16089273201 +6282331330243
+set hitung= +12055887525 +16123241945 +16089273201 
 Î
 (for %%a in (%hitung%) do (
 	echo %%a
 	python3 main.py %%a doge
-))
-Î
+	python3 main.py %%a btc
+	python3 main.py %%a bch
+	python3 main.py %%a ltc
+	python3 main.py %%a zec
+)
+)
+botcmd.bat
